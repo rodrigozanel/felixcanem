b1d70faba918ab93cbf8f8c0121afbad7d9c991a
Rodrigo Zanel <rodrigo.zanel-ext@atos.net>
2015-01-12 22:05:50 -0200

    add versao inicial

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<meta name="viewport" content="width=device-width, minimum-scale=0.1">
+<meta name="copyright" content="© 2014 Felix Canem" />
+<meta name="description" content="Passeadores de cães" />
+<meta name="keywords" content="Passeadores de c&atilde;es,dog,walker,dog walker,guarulhos,s&atilde;o paulo,sp,c&atilde;es,cachorros,passeadores, sa&uacute;de,animal,felicidade,felix,feliz,canem,caninos" />
+<title>Felix Canem</title>
+</head>
+<body style="margin: 0px; background-color: #04B431;">
+<div align="center">
+<img style="-webkit-user-select: none; cursor: -webkit-zoom-in;" src="10349977_636995059715378_790562208503848834_n.jpg" width="501" height="672" >
+</div>
+</body>
+</html>
